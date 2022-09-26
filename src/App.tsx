ba17b65a59ac2d876a1eebeec8c7d5238625018c
Neil Ghoshal <neilg.dev@gmail.com>
2022-09-26 10:06:15 -0400

    Initial project setup & commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+	return (
+		<div className="App">
+			<h1 className="text-4xl font-bold text-center text-blue-10">
+				Mostly Cloudy in PGH
+			</h1>
+		</div>
+	);
+}
+
+export default App;
