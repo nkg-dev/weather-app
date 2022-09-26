15451660446f6c75e8b6bea1b990f5a6276d75b7
Neil Ghoshal <neilg.dev@gmail.com>
2022-09-26 10:21:15 -0400

    Adds Search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Search from './components/Search';
+
 function App() {
 	return (
-		<div className="App">
+		<div className="max-w-5xl my-5 mx-auto">
 			<h1 className="text-4xl font-bold text-center text-blue-10">
 				Mostly Cloudy in PGH
 			</h1>
+			<Search />
 		</div>
 	);
 }
